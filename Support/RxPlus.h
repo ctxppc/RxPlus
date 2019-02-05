f1b04f27d5ff1750d039de55f480ede69816cee7
Constantino Tsarouhas <c@tsarouhas.eu>
2019-02-05 16:03:31 +0100

    Initial commit.

---
+++ Support/RxPlus.h
@@ -0,0 +1,9 @@
+// RxPlus © 2019 Creatunit
+
+@import UIKit;
+
+//! Project version number for RxPlus.
+FOUNDATION_EXPORT double RxPlusVersionNumber;
+
+//! Project version string for RxPlus.
+FOUNDATION_EXPORT const unsigned char RxPlusVersionString[];
